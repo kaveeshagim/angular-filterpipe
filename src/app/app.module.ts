0780cbc9582c4dc4899adf384c8ed1ec5f945e44
Kavee <kaveeshagw123456@gmail.com>
2023-08-10 22:23:00 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import {MatTableModule} from "@angular/material/table";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
